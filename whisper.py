9d0c163fd2c841d141df9fa8a8f74b77c13507ff
Aaryan YVS <51906812+Aaryan369@users.noreply.github.com>
2022-11-11 12:58:04 +0530

    Create whisper.py

---
+++ whisper.py
@@ -0,0 +1,14 @@
+import pysnooper
+
+@pysnooper.snoop('outputs/file.log')
+def main_func():
+    import os
+
+    file_path = 'Aaryan_A100/1minvid.webm'
+    
+    s = 'whisper ' + file_path + ' --language en --model large --output_dir outputs --condition_on_previous_text False --verbose False'
+
+    os.system(s)
+    
+
+main_func()
