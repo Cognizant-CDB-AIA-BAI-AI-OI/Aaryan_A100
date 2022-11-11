9bcab6f94142af7c2b0ad5113a18d6ce09c6a406
Aaryan YVS <51906812+Aaryan369@users.noreply.github.com>
2022-11-11 15:03:50 +0530

    aaryan whisper --run

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ def config():
   try :
     import sys 
     subprocess.run('conda env create -f Aaryan_A100/envs/whisper_env.yml',shell =True)
-    subprocess.run('conda run -n env_score python Aaryan_A100/whisper.py',shell =True)
+    subprocess.run('conda run -n whisper_env python Aaryan_A100/whisper.py',shell =True)
     subprocess.run('conda env remove -n whisper_env',shell =True)
  
   except :
